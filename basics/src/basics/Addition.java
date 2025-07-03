7d7ea3330234055f9d6f333c4d3051edaf4b24b9
srija <srija@Srijareddy>
2025-07-03 12:58:06 +0530

    UPDATED

--- basics/src/basics/Addition.java
+++ basics/src/basics/Addition.java
@@ -3,10 +3,10 @@ package basics;
 public class Addition {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub{
+		// TODO Auto-generated method stub
+		//ADD{
 		for(int i=0;i<=10;i++) {
 	    System.out.println(i);
 	}
-
 }
 }
