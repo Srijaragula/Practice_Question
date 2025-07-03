b711c3d952795da29a2e00192304ffdf4d553055
srija <srija@Srijareddy>
2025-07-03 13:01:14 +0530

    UPDATED1

--- basics/src/basics/Addition.java
+++ basics/src/basics/Addition.java
@@ -4,7 +4,8 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//ADD{
+		//ADDition
+		{
 		for(int i=0;i<=10;i++) {
 	    System.out.println(i);
 	}
