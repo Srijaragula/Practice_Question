c9b107c7e2920f5fa9810772feec33eac6349059
srija <srija@Srijareddy>
2025-07-03 13:08:32 +0530

    u2

--- basics/src/basics/Addition.java
+++ basics/src/basics/Addition.java
@@ -7,12 +7,8 @@ public class Addition {
 		// TODO Auto-generated method stub
 		//ADDition
 		{
-=======
-		// TODO Auto-generated method stub{
-		//ADDition
->>>>>>> branch 'master' of https://github.com/Srijaragula/Practice_Question.git
-		for(int i=0;i<=10;i++) {
-	    System.out.println(i);
+
 	}
 }
 }
+
